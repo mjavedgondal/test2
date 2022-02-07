ffd20567ef792d64c761633c7debeba7c1c88b91
mjavedgondal <99114004+mjavedgondal@users.noreply.github.com>
2022-02-07 17:36:31 +0500

    Add files via upload

---
+++ src/assets/icons/tokens/index.ts
@@ -0,0 +1,16 @@
+export { default as TRXLogo } from './tron-trx-logo.png';
+export { default as OSKLogo } from './osk-logo.png';
+export { default as WBTTLogo } from './wbtt-logo.png';
+export { default as JSTLogo } from './jst-logo.png';
+export { default as BTCLogo } from './btc-logo.png';
+export { default as SUNLogo } from './sun-logo.png';
+export { default as WINLogo } from './win-logo.png';
+export { default as TUSDLogo } from './tusd-logo.png';
+export { default as USDJLogo } from './usdj-logo.png';
+export { default as NFTLogo } from './nft-logo.png';
+export { default as USDCLogo } from './usdc-logo.png';
+export { default as ETHLogo } from './eth-logo.png';
+export { default as POSCHELogo } from './posche-logo.png';
+export { default as CNLogo } from './cn-logo.png';
+export { default as SafeMoneyLogo } from './safe-money-logo.png';
+export { default as DefaultLogo } from './default-logo.svg';
